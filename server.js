637f226536afdf516ee07741b3ff72d4cf7fcdc4
root <root@Zenbook>
2024-11-25 21:30:30 +0100

    test

--- server.js
+++ server.js
@@ -284,7 +284,7 @@ app.post('/api/kitchenLight/update', (req, res) => {
 
 
 // Start Server
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server running at http://0.0.0.0:${port}`);
 });
 
